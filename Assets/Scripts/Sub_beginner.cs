175ce5766082a652f861f6b79b9b9937e9d9989b
Harish Kunchala <124862025+harish-csusb@users.noreply.github.com>
2023-02-28 15:16:47 -0800

    changd the script

--- Assets/Scripts/Sub_beginner.cs
+++ Assets/Scripts/Sub_beginner.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Sub_beginner : MonoBehaviour
+
 {
-   public void navigate(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-    public void navigate_back(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-    }
 }
 
 
