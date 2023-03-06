282fd47c5a894a127f60a633b437a7c0a362f99c
Harish Kunchala <124862025+harish-csusb@users.noreply.github.com>
2023-03-06 07:27:57 -0800

    Moved the assets

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -1,12 +1,29 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
+/**
+ * This is going to be used to load all levels
+ */
 
 public class LevelLoader : MonoBehaviour
 {
 
 
-    public void LoadLevel(int sceneIndex)
+    public void LoadHomeScreen (int sceneIndex)
     {
-        SceneManager.LoadScene(sceneIndex);
+        StartCoroutine(LoadAsynchronously(sceneIndex));
     }
+
+    //This function is to just create a loading bar for splash screen to home screen
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+    }
+    
 }
