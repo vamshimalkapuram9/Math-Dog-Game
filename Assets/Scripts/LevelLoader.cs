d9ff8b4faf6f3d75a0f578e8043b069a9dbfacb1
Harish Kunchala <124862025+harish-csusb@users.noreply.github.com>
2023-03-07 09:44:32 -0800

    Added the Sub Quiz

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -3,10 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-
+    private int homePageIndex = 1;
 
     public void LoadLevel(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void GoToHomePage()
+    {
+        SceneManager.LoadScene(homePageIndex);
     }
 }
