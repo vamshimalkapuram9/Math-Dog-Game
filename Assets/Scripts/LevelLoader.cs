02ae9660eae3f544727375862f2fe95fda7c9465
Harish Kunchala <124862025+harish-csusb@users.noreply.github.com>
2023-03-07 09:33:07 -0800

    Added the updated Pravalika's screen

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -3,15 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-   private int homeScreenIndex = 1;
+
 
     public void LoadLevel(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
-    }
-
-    public void GoToHomeScreen()
-    {
-        SceneManager.LoadScene(homeScreenIndex);
     }
 }
