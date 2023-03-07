b3c363890ea84f972a6ab730e6251bf8b1c496d0
Harish Kunchala <124862025+harish-csusb@users.noreply.github.com>
2023-03-06 21:38:18 -0800

    added kids logo

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -3,9 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
+   private int homeScreenIndex = 1;
 
     public void LoadLevel(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void GoToHomeScreen()
+    {
+        SceneManager.LoadScene(homeScreenIndex);
     }
 }
