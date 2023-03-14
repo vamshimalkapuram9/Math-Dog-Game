082c4f9c6aff647f862c417cfd71383c9a795293
PravallikaMandru <008148420@coyote.csusb.edu>
2023-03-14 00:16:55 -0700

    Divya_screen

--- Assets/Scripts/SubHarish.cs
+++ Assets/Scripts/SubHarish.cs
@@ -1,43 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class SubHarish : MonoBehaviour
 {
-
-    public TextMeshProUGUI[] firstRandomNumbers;
-    public TextMeshProUGUI[] secondRandomNumbers;
-
-
-
     // Start is called before the first frame update
     void Start()
     {
-        GenerateRandomNumbers();
+        
     }
 
-    public void GenerateRandomNumbers()
+    // Update is called once per frame
+    void Update()
     {
-        int[] firstNosList = new int[6];
-
-        int randomNumber;
-
-        for (int i =0; i < 6; i ++)
-        {
-            randomNumber = Random.Range(2, 10);
-            firstRandomNumbers[i].text = randomNumber.ToString();
-            firstNosList[i] = randomNumber;
-        }
-
-        for (int i = 0; i < 6; i++)
-        {
-            randomNumber = Random.Range(1, firstNosList[i]);
-
-            secondRandomNumbers[i].text = randomNumber.ToString();
-
-        }
+        
     }
-
 }
