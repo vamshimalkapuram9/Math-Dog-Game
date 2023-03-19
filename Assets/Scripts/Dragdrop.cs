39dc623dc27b23b09267a3e430b798f53ad9b0dc
Alekhya1636 <008168830@coyote.csusb.edu>
2023-03-18 17:57:41 -0700

    added code for random numbers

---
+++ Assets/Scripts/Dragdrop.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dragdrop : MonoBehaviour
+{
+
+    public GameObject Crt_ans;
+    public GameObject AnsB;
+    public Text Ans;
+    public Text num1;
+    public Text num2;
+
+    public float dropdistance;
+
+    public bool islocked;
+
+    Vector2 objectInitPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectInitPos = AnsB.transform.position;
+    }
+    public void DragObject()
+    {
+        if (!islocked)
+        {
+            AnsB.transform.position = Input.mousePosition;
+        }
+    }
+    public void DropObject() 
+    {
+        int a = Convert.ToInt32(num1.text);
+        int b = Convert.ToInt32(num2.text);
+        int c = Convert.ToInt32(Ans.text);
+        float Distance = Vector3.Distance(AnsB.transform.position, Crt_ans.transform.position);
+        if (c == a - b)
+        {
+            islocked = true;
+            AnsB.transform.position = Crt_ans.transform.position;
+        }
+        else 
+        {
+            islocked = false;
+            AnsB.transform.position = objectInitPos;
+        }
+    }
+}
---
+++ Assets/Scripts/Random_numbers.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Random_numbers : MonoBehaviour
+{
+    public TextMeshProUGUI[] firstRandomNumbers;
+    public TextMeshProUGUI[] secondRandomNumbers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateRandomNumbers();
+    }
+    public void GenerateRandomNumbers()
+    {
+        int[] firstNosList = new int[6];
+        int randomNumber;
+        for (int i =0; i < 6; i ++)
+        {
+            randomNumber = Random.Range(1, 6);
+            firstRandomNumbers[i].text = randomNumber.ToString();
+            firstNosList[i] = randomNumber;
+        }
+        for (int i = 0; i < 6; i++)
+        {
+            randomNumber = Random.Range(1, firstNosList[i]);
+            secondRandomNumbers[i].text = randomNumber.ToString();
+        }
+    }
+}
