cd842b1af08100c52d38380872052c921be3b818
Harish Kunchala <124862025+harish-csusb@users.noreply.github.com>
2023-04-03 11:46:38 -0700

    Sub Intermediate

--- Assets/Scripts/Harish-Code/DraggableButton.cs
+++ Assets/Scripts/Harish-Code/DraggableButton.cs
@@ -1,27 +1,81 @@
-    using UnityEngine;
-    using UnityEngine.EventSystems;
 
-    public class DraggableButton : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+using System;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DraggableButton : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+private Vector2 originalPosition;
+
+public GameObject answerPanelObject;
+
+    public TextMeshProUGUI FirstNumberText;
+    public TextMeshProUGUI SecondNumberText;
+    public TextMeshProUGUI TextInAnswerPanel;
+    public Text answerText;
+
+void Start()
     {
-    private Vector3 originalPosition;
-
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-    originalPosition = transform.position;
+        TextInAnswerPanel.enabled = false;
     }
 
-    public void OnDrag(PointerEventData eventData)
-    {
-    transform.position = Input.mousePosition;
-    }
 
-    public void OnEndDrag(PointerEventData eventData)
+public void OnBeginDrag(PointerEventData eventData)
+{
+originalPosition = transform.position;
+}
+
+public void OnDrag(PointerEventData eventData)
+{
+transform.position = Input.mousePosition;
+}
+
+public void OnEndDrag(PointerEventData eventData)
+    {
+
+        //transform.position = eventData.pointerCurrentRaycast.gameObject.transform.position;
+
+        int firstNo = Convert.ToInt32(FirstNumberText.text);
+        int secondNo = Convert.ToInt32(SecondNumberText.text);
+
+        int givenValue = Convert.ToInt32(answerText.text);
+
+        int correctAnswerValue = firstNo - secondNo;
+
+
+        if(givenValue == correctAnswerValue)
         {
+            transform.position = answerPanelObject.transform.position;
 
-    transform.position = eventData.pointerCurrentRaycast.gameObject.transform.position;
+            StartCoroutine(WaitTwoSeconds());
+           
+            TextInAnswerPanel.text = correctAnswerValue.ToString();
 
+            TextInAnswerPanel.enabled = true;
+
+            
+        }
+
+        else
+        {
+            transform.position = originalPosition;
+        }
+
+             
+}
+    IEnumerator WaitTwoSeconds()
+    {
+        Debug.Log("Coroutine started");
+        yield return new WaitForSeconds(2);
+        Debug.Log("Two seconds have passed");
+
+        transform.position = originalPosition;
 
     }
-    
-    }
+
+
+
+}
