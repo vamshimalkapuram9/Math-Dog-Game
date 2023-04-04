dfaf23617b8ffee921209d66003c43ed7cf8f200
Harish Kunchala <124862025+harish-csusb@users.noreply.github.com>
2023-04-04 08:01:11 -0700

    Set the base code for divya

--- Assets/Scripts/Divya-Code/DivyaDragDrop.cs
+++ Assets/Scripts/Divya-Code/DivyaDragDrop.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DivyaDragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class DivyaDragDrop : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
