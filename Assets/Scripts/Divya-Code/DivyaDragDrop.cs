7645548c2980ccaf74ea5d4cb0dbfd4ab1aa1c13
Divya Madireddy <124994006+Alekhya1636@users.noreply.github.com>
2023-04-04 10:15:44 -0700

    modified the screen

--- Assets/Scripts/Divya-Code/DivyaDragDrop.cs
+++ Assets/Scripts/Divya-Code/DivyaDragDrop.cs
@@ -1,35 +1,84 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
-public class DivyaDragDrop : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler
+using TMPro;
+using System;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DivyaDragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private Vector2 originalPosition;
+    public Divya_randomnumbers Script;
+    GameObject answerPanelObject;
+    TextMeshProUGUI FirstNumberText;
+    TextMeshProUGUI SecondNumberText;
+    TextMeshProUGUI TextInAnswerPanel;
+    public Text buttonText;
+    GameObject PresentlyActivePanel;
+    void Start()
+    {
+
+    }
+    // void CheckPanel()
+    //{
+    //    Transform firstChild = PresentlyActivePanel.transform.GetChild(0);
+    //    TextMeshProUGUI firstText = firstChild.GetComponent<TextMeshProUGUI>();
+
+    //    Debug.Log("drag " + firstText.text);
+    //    //Debug.Log(PresentlyActivePanel.transform.childCount);
+    //}
+    void initTextsAndVariables()
+    {
+        GameObject cPanel = Script.getCurrentlyActivePanel();
+        Transform firstChild = cPanel.transform.GetChild(0);
+        //FirstNumberText TMP
+        FirstNumberText = firstChild.GetComponent<TextMeshProUGUI>();
+        Transform secondChild = cPanel.transform.GetChild(2);
+        //SecondNumberText TMP
+        SecondNumberText = secondChild.GetComponent<TextMeshProUGUI>();
+        //Debug.Log(" "+cPanel.transform.GetChild(4).tag);
+        answerPanelObject = cPanel.transform.GetChild(4).gameObject;
+        //TIA Code
+        Transform TIATransform = answerPanelObject.transform.GetChild(0);
+        TextInAnswerPanel = TIATransform.GetComponent<TextMeshProUGUI>();
+        TextInAnswerPanel.enabled = false;
+        Debug.Log("TIA " + TextInAnswerPanel.text);
+    }
     public void OnBeginDrag(PointerEventData eventData)
     {
+        initTextsAndVariables();
         originalPosition = transform.position;
     }
-
     public void OnDrag(PointerEventData eventData)
     {
         transform.position = Input.mousePosition;
     }
-
     public void OnEndDrag(PointerEventData eventData)
     {
-        transform.position = Input.mousePosition;
+        //transform.position = eventData.pointerCurrentRaycast.gameObject.transform.position;
+        int firstNo = Convert.ToInt32(FirstNumberText.text);
+        int secondNo = Convert.ToInt32(SecondNumberText.text);
+        int givenValue = Convert.ToInt32(buttonText.text);
+        int correctAnswerValue = firstNo - secondNo;
+        if (givenValue == correctAnswerValue)
+        {
+            transform.position = answerPanelObject.transform.position;
+            StartCoroutine(WaitTwoSeconds());
+            TextInAnswerPanel.text = correctAnswerValue.ToString();
+            TextInAnswerPanel.enabled = true;
+        }
+        else
+        {
+            transform.position = originalPosition;
+        }
     }
-
-    // Start is called before the first frame update
-    void Start()
+    IEnumerator WaitTwoSeconds()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Coroutine started");
+        yield return new WaitForSeconds(2);
+        Debug.Log("Two seconds have passed");
+        transform.position = originalPosition;
+        Script.changeCurrentlyActivePanel();
     }
 }
