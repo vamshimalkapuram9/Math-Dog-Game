2e4cab19ec6626a9e628fdc9d2ddb163b68cc285
Harish Kunchala <124862025+harish-csusb@users.noreply.github.com>
2023-04-03 20:34:24 -0700

    Previous code for DraggableButton
    This is the previous code for my DraggableButton.cs file

--- Assets/Scripts/Harish-Code/DraggableButton.cs
+++ Assets/Scripts/Harish-Code/DraggableButton.cs
@@ -1,18 +1,104 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class DraggableButton : MonoBehaviour
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+using System;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DraggableButton : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called before the first frame update
+    private Vector2 originalPosition;
+
+
+    public GameObject answerPanelObject;
+
+    public TextMeshProUGUI FirstNumberText;
+    public TextMeshProUGUI SecondNumberText;
+    public TextMeshProUGUI TextInAnswerPanel;
+    public Text answerText;
+
+    GameObject PresentlyActivePanel;
+
+
+
     void Start()
     {
-        
+        TextInAnswerPanel.enabled = false;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void CheckPanel()
     {
-        
+
+
+
+        Transform firstChild = PresentlyActivePanel.transform.GetChild(0);
+
+        TextMeshProUGUI firstText = firstChild.GetComponent<TextMeshProUGUI>();
+
+        Debug.Log("drag " + firstText.text);
+        //Debug.Log(PresentlyActivePanel.transform.childCount);
+
+
+
     }
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalPosition = transform.position;
+        CheckPanel();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+        //transform.position = eventData.pointerCurrentRaycast.gameObject.transform.position;
+
+        int firstNo = Convert.ToInt32(FirstNumberText.text);
+        int secondNo = Convert.ToInt32(SecondNumberText.text);
+
+        int givenValue = Convert.ToInt32(answerText.text);
+
+        int correctAnswerValue = firstNo - secondNo;
+
+
+        if (givenValue == correctAnswerValue)
+        {
+            transform.position = answerPanelObject.transform.position;
+
+            StartCoroutine(WaitTwoSeconds());
+
+            TextInAnswerPanel.text = correctAnswerValue.ToString();
+
+            TextInAnswerPanel.enabled = true;
+
+
+        }
+
+        else
+        {
+            transform.position = originalPosition;
+        }
+
+
+    }
+    IEnumerator WaitTwoSeconds()
+    {
+        Debug.Log("Coroutine started");
+        yield return new WaitForSeconds(2);
+        Debug.Log("Two seconds have passed");
+
+        transform.position = originalPosition;
+
+    }
+
+
+
 }
