ab62cfd7519fdc686c0c79c6df24800709c377ea
Divya Madireddy <124994006+Alekhya1636@users.noreply.github.com>
2023-04-04 08:11:34 -0700

    Added drag and drop

--- Assets/Scripts/Divya-Code/DivyaDragDrop.cs
+++ Assets/Scripts/Divya-Code/DivyaDragDrop.cs
@@ -1,9 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class DivyaDragDrop : MonoBehaviour
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class DivyaDragDrop : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler
 {
+    private Vector2 originalPosition;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalPosition = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
