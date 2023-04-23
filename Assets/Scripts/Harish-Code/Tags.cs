bae88612a5c9eab2ef3f868a4742aa6596edc72d
Harish Kunchala <124862025+harish-csusb@users.noreply.github.com>
2023-04-22 17:53:04 -0700

    Tags added for objects

--- Assets/Scripts/Harish-Code/Tags.cs
+++ Assets/Scripts/Harish-Code/Tags.cs
@@ -18,6 +18,21 @@ public static class Tags
     public const string ANS5_BTN = "Ans5Btn";
     public const string ANS6_BTN = "Ans6Btn";
 
+
+    //Invisible Objects in Sub FUN
+
+    public const string FUN_OBJECT_1 = "1Object";
+    public const string FUN_OBJECT_2 = "2Object";
+    public const string FUN_OBJECT_3 = "3Object";
+    public const string FUN_OBJECT_4 = "4Object";
+    public const string FUN_OBJECT_5 = "5Object";
+    public const string FUN_OBJECT_6 = "6Object";
+    public const string FUN_OBJECT_7 = "7Object";
+    public const string FUN_OBJECT_8 = "8Object";
+    public const string FUN_OBJECT_9 = "9Object";
+    public const string FUN_OBJECT_10 = "10Object";
+
+
     // Panel with The Answer Buttons
     public const string ANSWER_BUTTONS_PANEL = "AnswerButtonsPanel";
 
