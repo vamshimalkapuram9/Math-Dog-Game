fe5903f331bc8879865741ae93a3ea7aff0107e1
Navitha-Nelluri <124995352+Navitha-Nelluri@users.noreply.github.com>
2023-04-24 01:38:22 -0700

    voiceover added

---
+++ Assets/Scripts/Dogvoice.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Dogvoice : MonoBehaviour
+{
+    public AudioClip clip;
+    public AudioClip clip1;
+    public AudioClip clip2;
+    public AudioClip clip3;
+    public AudioClip clip4;
+    public AudioClip clip5;
+    public AudioClip clip6;
+    public AudioClip clip7;
+    public AudioClip clip8;
+    public AudioClip clip9;
+    public AudioClip clip10;
+    public AudioClip clip11;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayClipForButton(Button button)
+    {
+        string buttonText = button.GetComponentInChildren<Text>().text;
+        switch (buttonText)
+        {
+            case "0":
+                audioSource.clip = clip1;
+                break;
+            case "1":
+                audioSource.clip = clip2;
+                break;
+            case "2":
+                audioSource.clip = clip3;
+                break;
+            case "3":
+                audioSource.clip = clip4;
+                break;
+            case "4":
+                audioSource.clip = clip5;
+                break;
+            case "5":
+                audioSource.clip = clip6;
+                break;
+            case "6":
+                audioSource.clip = clip7;
+                break;
+            case "7":
+                audioSource.clip = clip8;
+                break;
+            case "8":
+                audioSource.clip = clip9;
+                break;
+            case "9":
+                audioSource.clip = clip10;
+                break;
+            case "10":
+                audioSource.clip = clip11;
+                break;
+            default:
+                Debug.LogWarning("No clip found for button text: " + buttonText);
+                return;
+        }
+        audioSource.Play();
+    }
+}
