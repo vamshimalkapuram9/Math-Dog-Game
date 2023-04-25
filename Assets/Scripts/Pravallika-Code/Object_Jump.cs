61d69356ab084f96059da8e88c9fac7e5166ebf9
PravallikaMandru <008148420@coyote.csusb.edu>
2023-04-25 00:02:56 -0700

    wobble effect in sub_fun

--- Assets/Scripts/Pravallika-Code/Object_Jump.cs
+++ Assets/Scripts/Pravallika-Code/Object_Jump.cs
@@ -1,23 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class Object_Jump : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    public float jump = 10f;
+    public float amp;
+
+    public GameObject Object_;
+
+    public float x; 
+
+    public float y;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    //void Start()
+    //{
+    //    Object_ = Ans1.transform.position;
+    //}
 
     // Update is called once per frame
     void Update()
     {
-        if (true)
-        {
-            rb.AddForce(new Vector2(rb.velocity.x, jump));
-        }
+        transform.position = new Vector3(x, (Mathf.Sin(Time.time) * amp) + y, 0);
+        
     }
 }
